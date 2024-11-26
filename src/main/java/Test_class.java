0948e16d7fc3bcd22bbe4651d8bace403c7ce9aa
Nastya Lyuleva <153557808+Defoltini@users.noreply.github.com>
2024-11-27 00:06:12 +0300

    delete

--- src/main/java/Test_class.java
+++ src/main/java/Test_class.java
@@ -1,7 +1,6 @@
 public class Test_class {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is Nastya and maybe Stepan!!!!");
-        System.out.println("Bye");
+       
 
     }
 }
