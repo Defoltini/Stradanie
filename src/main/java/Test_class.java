c8918a87d3b382dc1719ee61081a41dd77547add
Anastasia Lyuleva <nesti-1998@mail.ru>
2024-11-27 00:04:24 +0300

    update text

--- src/main/java/Test_class.java
+++ src/main/java/Test_class.java
@@ -1,7 +1,7 @@
 public class Test_class {
     public static void main(String[] args) {
         System.out.println("Hello! My name is Nastya!");
-        System.out.println("Bye!!!");
+        System.out.println("Bye!!!")
 
     }
 }
