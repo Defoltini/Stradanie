d04c95f2b7407260a4432a3dad02abd2e4246580
Anastasia Lyuleva <nesti-1998@mail.ru>
2024-11-26 23:44:27 +0300

    update text string 4

--- src/main/java/Test_class.java
+++ src/main/java/Test_class.java
@@ -1,6 +1,6 @@
 public class Test_class {
     public static void main(String[] args) {
-        String message = "Hello! My name Natya!"
+        String message = "Hello! My name Natya"
         System.out.println(message);
         System.out.println("Bye");
         
