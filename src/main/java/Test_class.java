f8675ffad05aaf0e6c72104345c20699af52a4d6
Anastasia Lyuleva <nesti-1998@mail.ru>
2024-11-26 23:56:05 +0300

    delete text

--- src/main/java/Test_class.java
+++ src/main/java/Test_class.java
@@ -2,6 +2,5 @@ public class Test_class {
     public static void main(String[] args) {
         String message = "Hello! My name Stepan"
         System.out.println(message);
-        System.out.println("Bye");
     }
 }
