1df9a7e55924d37203a303f6a57bfdbc598c65f0
Nastya Lyuleva <153557808+Defoltini@users.noreply.github.com>
2024-11-26 23:36:55 +0300

    Update text

--- src/main/java/Test_class.java
+++ src/main/java/Test_class.java
@@ -1,6 +1,8 @@
 public class Test_class {
     public static void main(String[] args) {
-        String message = "Hello! My name is Nastya and maybe Stepan!"
+        String message = "Hello! My name Natya!"
         System.out.println(message);
+        System.out.println("Bye");
+        
     }
 }
